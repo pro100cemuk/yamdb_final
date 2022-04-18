f1537003c19464e10a83978ade50d472f48997e0
Семенов Евгений Игоревич <Semenov.EI@GRP.LOCAL>
2022-04-18 17:44:53 +0300

    first review sending

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -117,7 +117,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static')),
                     ('redoc', '/app/templates/')]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
