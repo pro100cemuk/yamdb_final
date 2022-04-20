fab1764f0c4962ffe38faf3a5472502b8163bff0
Семенов Евгений Игоревич <Semenov.EI@GRP.LOCAL>
2022-04-20 10:39:24 +0300

    changed workflow

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -114,10 +114,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static')),
-#                     ('redoc', '/app/templates/')]
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static')),
+                     ('redoc', '/app/templates/')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
